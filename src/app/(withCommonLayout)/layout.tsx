12c6532ff36036049ae0f0e3978094a1217ac47a
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-05-02 19:06:13 +0600

    heroSection 1st step created

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,11 +1,15 @@
+import Footer from "@/components/shared/Footer/Footer";
 import Navbar from "@/components/shared/Navbar/Navbar";
 
 const CommonLayout = ({children}:{children:React.ReactNode}) => {
     return (
-        <div>
+        <>
             <Navbar />
+            <div className="min-h-screen">
             {children}
-        </div>
+            </div>
+            <Footer />
+        </>
     );
 };
 
