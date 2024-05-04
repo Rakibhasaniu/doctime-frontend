6ede0738f153f3412e5dd5c1d8f0254fef9e136a
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-05-04 10:02:04 +0600

    register page created

---
+++ src/app/register/page.tsx
@@ -0,0 +1,116 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
+import Link from "next/link";
+
+const RegisterPage = () => {
+  return (
+    <Container sx={{ padding: "50px" }}>
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          // height:"100vh"
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+              // height:"100vh"
+            }}
+          >
+            <Box>
+              <Image
+                src={assets.svgs.logo}
+                width={50}
+                height={50}
+                alt="background-image"
+              />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <form>
+              <Grid container spacing={2} my={1}>
+                <Grid item md={12}>
+                  <TextField
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Contact Number"
+                    type="tel"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Address"
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+              </Grid>
+              <Button sx={{ margin: "10px 0px" }} fullWidth={true}>
+                Register
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Do you already have an account? <Link href="/login">Login</Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
+  );
+};
+
+export default RegisterPage;
