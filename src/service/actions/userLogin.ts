c4500313707d68793d524c8b3a14f00eba3f526b
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-05-11 22:38:44 +0600

    refresh token set cookies

--- src/service/actions/userLogin.ts
+++ src/service/actions/userLogin.ts
@@ -1,4 +1,4 @@
-"use server";
+// "use server";
 import { FieldValues } from "react-hook-form";
 
 export const userLogin = async (data: FieldValues) => {
@@ -10,7 +10,8 @@ export const userLogin = async (data: FieldValues) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-      cache: "no-store",
+      // cache: "no-store",
+      credentials:"include"
     }
   );
   const userInfo = await res.json();
--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,4 +1,5 @@
 import { authKey } from "@/constants/authKey";
+import { instance as axiosInstance } from "@/helpers/axios/axiosInstance";
 import { decodedToken } from "@/utils/jwt";
 import { getFromLocalStorage, removeFromLocalStorage, setToLocalStorage } from "@/utils/local-storage";
 
@@ -30,3 +31,14 @@ export const isLoggedIn = () => {
 export const removeUser = () => {
   return removeFromLocalStorage(authKey);
 };
+
+export const getNewAccessToken = async() => {
+  return await axiosInstance({
+    url:"http://localhost:5000/api/v1/auth/refresh-token",
+    method:"POST",
+    headers:{
+      "Content-Type": "application/json",  
+    },
+    withCredentials:true
+  })
+}
